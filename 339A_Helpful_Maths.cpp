35db3aa5c3cc1fc4522043e5f8c7ab8a0195ff23
amanosan121 <amanosan121@gamil.com>
2020-12-24 00:38:38 +0530

    added another solution

--- 339A_Helpful_Maths.cpp
+++ 339A_Helpful_Maths.cpp
@@ -21,6 +21,41 @@ string helpfulMaths(string str)
     return result;
 }
 
+// another solution
+string helpfulMaths2(string str)
+{
+    string result;
+    int count1 = 0, count2 = 0, count3 = 0;
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (str[i] == '1')
+            count1++;
+        else if (str[i] == '2')
+            count2++;
+        else if (str[i] == '3')
+            count3++;
+        else
+            continue;
+    }
+    for (int i = 0; i < count1; i++)
+    {
+        result.push_back('1');
+        result.push_back('+');
+    }
+    for (int i = 0; i < count2; i++)
+    {
+        result.push_back('2');
+        result.push_back('+');
+    }
+    for (int i = 0; i < count3; i++)
+    {
+        result.push_back('3');
+        result.push_back('+');
+    }
+    result.pop_back();
+    return result;
+}
+
 int main()
 {
     string str;
